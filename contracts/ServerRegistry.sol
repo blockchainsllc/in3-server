a6226c48370d993a7f74af611cb97928a0a98675
Simon Jentzsch <simon@slock.it>
2018-06-14 20:34:05 +0200

    removed warnings

--- contracts/ServerRegistry.sol
+++ contracts/ServerRegistry.sol
@@ -31,7 +31,7 @@ contract ServerRegistry {
     /// register a new Server with the sender as owner    
     function registerServer(string _url, uint _props) public payable {
         // make sure this url and also this owner was not registered before.
-        var hash = keccak256(_url);
+        bytes32 hash = keccak256(_url);
         for (uint i=0;i<servers.length;i++) 
             require(keccak256(servers[i].url)!=hash && servers[i].owner!=msg.sender);
 
@@ -42,7 +42,7 @@ contract ServerRegistry {
         m.owner = msg.sender;
         m.deposit = msg.value;
         servers.push(m);
-        LogServerRegistered(_url, _props, msg.sender,msg.value);
+        emit LogServerRegistered(_url, _props, msg.sender,msg.value);
     }
 
     /// this should be called before unregistering a server.
@@ -55,7 +55,7 @@ contract ServerRegistry {
     //       if the owner become active again 
     //       or the caller will receive 20% of the deposit in case the owner does not react.
     function requestUnregisteringServer(uint _serverIndex) payable public {
-        var server = servers[_serverIndex];
+        Web3Server storage server = servers[_serverIndex];
         // this can only be called if nobody requested it before
         require(server.unregisterCaller==address(0x0));
 
@@ -67,15 +67,15 @@ contract ServerRegistry {
             require(msg.value==unregisterDeposit);
         }
         server.unregisterCaller = msg.sender;
-        LogServerUnregisterRequested(server.url, server.owner, msg.sender );
+        emit LogServerUnregisterRequested(server.url, server.owner, msg.sender );
     }
     
     function confirmUnregisteringServer(uint _serverIndex) public {
-        var server = servers[_serverIndex];
+        Web3Server storage server = servers[_serverIndex];
         // this can only be called if somebody requested it before
         require(server.unregisterCaller!=address(0x0) && server.unregisterTime < now);
 
-        var payBackOwner = server.deposit;
+        uint payBackOwner = server.deposit;
         if (server.unregisterCaller != server.owner) {
             payBackOwner -= server.deposit/5;  // the owner will only receive 80% of his deposit back.
             server.unregisterCaller.transfer( unregisterDeposit + server.deposit - payBackOwner );
@@ -88,7 +88,7 @@ contract ServerRegistry {
     }
 
     function cancelUnregisteringServer(uint _serverIndex) public {
-        var server = servers[_serverIndex];
+        Web3Server storage server = servers[_serverIndex];
 
         // this can only be called by the owner and if somebody requested it before
         require(server.unregisterCaller!=address(0) &&  server.owner == msg.sender);
@@ -101,7 +101,7 @@ contract ServerRegistry {
         server.unregisterCaller = address(0);
         server.unregisterTime = 0;
         
-        LogServerUnregisterCanceled(server.url, server.owner);
+        emit LogServerUnregisterCanceled(server.url, server.owner);
     }
 
 
@@ -122,7 +122,7 @@ contract ServerRegistry {
             address(0).transfer(servers[_serverIndex].deposit-payout);
         }
 
-        LogServerConvicted(servers[_serverIndex].url, servers[_serverIndex].owner );
+        emit LogServerConvicted(servers[_serverIndex].url, servers[_serverIndex].owner );
         removeServer(_serverIndex);
 
     }
@@ -130,7 +130,7 @@ contract ServerRegistry {
     // internal helpers
     
     function removeServer(uint _serverIndex) internal {
-        LogServerRemoved(servers[_serverIndex].url, servers[_serverIndex].owner );
+        emit LogServerRemoved(servers[_serverIndex].url, servers[_serverIndex].owner );
         uint length = servers.length;
         Web3Server memory m = servers[length - 1];
         servers[_serverIndex] = m;
