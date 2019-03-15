e5045e22e335438c70ebaf55391987af76e5a86a
Martin Küchler <mkuechle@hs-mittweida.de>
2019-03-15 10:43:32 +0100

    updated BlockhashRegistry contract

--- contracts/BlockhashRegistry.sol
+++ contracts/BlockhashRegistry.sol
@@ -23,6 +23,18 @@ contract BlockhashRegistry {
         emit LogBlockhashAdded(prevBlockNumber, bhash);
     }
 
+
+    // stores a certain blockhash to the state
+    function saveBlockNumber(uint _blockNumber) public {
+        bytes32 bHash = blockhash(_blockNumber);
+
+        require(bHash != 0x0,"block not available");
+
+        blockhashMapping[_blockNumber] = bHash;
+        emit LogBlockhashAdded(_blockNumber, bHash);
+
+    }
+
     function getParentAndBlockhash(bytes memory _blockheader) public pure returns (bytes32 parentHash, bytes32 bhash) {
 
         // first bytes as uint 
@@ -47,36 +59,60 @@ contract BlockhashRegistry {
         bhash = keccak256(_blockheader);
     }
 
-    function calculateBlockheaders(bytes[] memory _blockheaders, bytes32 _startHash) public view returns (bytes32 bhash) {
+    // starts with a given blockhash and adds earlier blocks
+    function calculateBlockheadersFrom(bytes[] memory _blockheaders, bytes32 _bHash) public view returns (bytes32 bhash) {
 
-        bytes32 currentBlockhash = _startHash;
-
+        bytes32 currentBlockhash = _bHash;
+        bytes32 calcParent = 0x0;
+        bytes32 calcBlockhash = 0x0;
         for(uint i = 0; i < _blockheaders.length; i++) {
 
-            (bytes32 calcParent, bytes32 calcBlockhash) = getParentAndBlockhash(_blockheaders[i]);
-
+            (calcParent, calcBlockhash) = getParentAndBlockhash(_blockheaders[i]);
             if(calcBlockhash != currentBlockhash) return 0x0;
-
             currentBlockhash = calcParent;
         }
 
         return currentBlockhash;
     }
 
-    function recreateBlockheadersFrom(uint _startBlocknumber, bytes[] memory _blockheaders) public {
-        bytes32 currentBlockhash = blockhashMapping[_startBlocknumber];
+    function recreateBlockheadersFrom(uint _blockNumber, bytes[] memory _blockheaders) public {
+        bytes32 currentBlockhash = blockhashMapping[_blockNumber];
         require(currentBlockhash != 0x0, "parentBlock is not available");
 
-        bytes32 calculatedHash = calculateBlockheaders(_blockheaders, currentBlockhash);
-
+        bytes32 calculatedHash = calculateBlockheadersFrom(_blockheaders, currentBlockhash);
         require(calculatedHash != 0x0, "invalid headers");
 
-        uint bnr = _startBlocknumber - _blockheaders.length;
-
+        uint bnr = _blockNumber - _blockheaders.length;
         blockhashMapping[bnr] = calculatedHash;
         emit LogBlockhashAdded(bnr, calculatedHash);
-
-
     }
+
+    // starts with some blockheaders and ends with a given blockhash
+    function calculateBlockheadersTo(bytes[] memory _blockheaders, bytes32 _bHash) public view returns (bytes32 bhash) {
+
+        (bytes32 newBlockhash, bytes32 currentBlockhash) = getParentAndBlockhash(_blockheaders[0]);
+
+        for(uint i = 1; i < _blockheaders.length; i++) {
+
+            (bytes32 calcParent, bytes32 calcBlockhash) = getParentAndBlockhash(_blockheaders[i]);
+            if(calcParent != currentBlockhash) return 0x0;
+            currentBlockhash = calcBlockhash;
+        }
+
+        if(currentBlockhash != _bHash) return 0x0;
+
+        return newBlockhash;
+    }
+
+    function recreateBlockheadersTo(uint _blockNumber, bytes[] memory _blockheaders) public {
+        bytes32 endBlockhash = blockhashMapping[_blockNumber];
+        require(endBlockhash != 0x0, "endBlock is not available");
+        require(calculateBlockheadersTo(_blockheaders, endBlockhash) != 0x0, "invalid headers");
+
+        uint bnr = _blockNumber - _blockheaders.length;
+        blockhashMapping[bnr] = endBlockhash;
+        emit LogBlockhashAdded(bnr, endBlockhash);
+    }
+
 
 }
