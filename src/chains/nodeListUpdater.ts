64398fe4d4beafd6aef3da26cf7409033fca5984
Martin Küchler <mkuechle@hs-mittweida.de>
2019-03-19 13:57:41 +0100

    updatd nodeListUpdated for contract changes

--- src/chains/nodeListUpdater.ts
+++ src/chains/nodeListUpdater.ts
@@ -17,15 +17,15 @@
 * For questions, please contact info@slock.it              *
 ***********************************************************/
 
-import { RPCHandler }                              from '../server/rpc'
-import * as tx                                     from '../util/tx'
-import * as abi                                    from 'ethereumjs-abi'
+import { RPCHandler } from '../server/rpc'
+import * as tx from '../util/tx'
+import * as abi from 'ethereumjs-abi'
 import { createRandomIndexes, Proof, ServerList, BlockData, AccountProof, RPCRequest, IN3NodeConfig, util, storage, serialize } from 'in3'
-import { toChecksumAddress, keccak256, toBuffer }  from 'ethereumjs-util'
+import { toChecksumAddress, keccak256, toBuffer } from 'ethereumjs-util'
 
-const toHex    = util.toHex
+const toHex = util.toHex
 const toBuffer = util.toBuffer
-const bytes32  = serialize.bytes32
+const bytes32 = serialize.bytes32
 
 /** returns a nodelist filtered by the given params and proof. */
 export async function getNodeList(handler: RPCHandler, nodeList: ServerList, includeProof = false, limit = 0, seed?: string, addresses: string[] = []): Promise<ServerList> {
@@ -45,10 +45,10 @@ export async function getNodeList(handler: RPCHandler, nodeList: ServerList, inc
     createRandomIndexes(nodes.length, limit, bytes32(seed), result)
 
     const nl: ServerList = {
-      totalServers   : nodeList.totalServers,
-      contract       : nodeList.contract,
+      totalServers: nodeList.totalServers,
+      contract: nodeList.contract,
       lastBlockNumber: nodeList.lastBlockNumber,
-      nodes          : result.map(i => nodeList.nodes[i])
+      nodes: result.map(i => nodeList.nodes[i])
     }
 
     if (includeProof) {
@@ -107,9 +107,9 @@ export async function createNodeListProof(handler: RPCHandler, nodeList: ServerL
   const keys: Buffer[] = getStorageKeys(nodeList.nodes)
 
   // TODO maybe we should use a block that is 6 blocks old since nobody would sign a blockhash for latest.
-  const address   = nodeList.contract
-  const lastBlock = await handler.getFromServer({ method:'eth_blockNumber', params:[] }).then(_=>parseInt(_.result))
-  const blockNr   = lastBlock ? '0x'+Math.max(nodeList.lastBlockNumber,lastBlock -  (handler.config.minBlockHeight || 0)).toString(16) : 'latest'
+  const address = nodeList.contract
+  const lastBlock = await handler.getFromServer({ method: 'eth_blockNumber', params: [] }).then(_ => parseInt(_.result))
+  const blockNr = lastBlock ? '0x' + Math.max(nodeList.lastBlockNumber, lastBlock - (handler.config.minBlockHeight || 0)).toString(16) : 'latest'
 
   // read the response,blockheader and trace from server
   const [blockResponse, proof] = await handler.getAllFromServer([
@@ -117,24 +117,25 @@ export async function createNodeListProof(handler: RPCHandler, nodeList: ServerL
     { method: 'eth_getProof', params: [toHex(address, 20), keys.map(_ => toHex(_, 32)), blockNr] }
   ])
 
+  // console.log(proof.result.storageProof.map(_ => _.key + ' = ' + _.value).join('\n'))
   // error checking
   if (blockResponse.error) throw new Error('Could not get the block for ' + blockNr + ':' + blockResponse.error)
-  if (proof.error)         throw new Error('Could not get the proof :' + JSON.stringify(proof.error, null, 2) + ' for request ' + JSON.stringify({ method: 'eth_getProof', params: [toHex(address, 20), keys.map(toHex), blockNr] }, null, 2))
+  if (proof.error) throw new Error('Could not get the proof :' + JSON.stringify(proof.error, null, 2) + ' for request ' + JSON.stringify({ method: 'eth_getProof', params: [toHex(address, 20), keys.map(toHex), blockNr] }, null, 2))
 
 
   // make sure we use minHex for the proof-keys
   if (proof.result && proof.result.storageProof)
-  proof.result.storageProof.forEach(p=>p.key=util.toMinHex(p.key))
-    
+    proof.result.storageProof.forEach(p => p.key = util.toMinHex(p.key))
+
 
   // anaylse the transaction in order to find all needed storage
-  const block   = blockResponse.result as BlockData
+  const block = blockResponse.result as BlockData
   const account = proof.result as AccountProof
 
   // bundle the answer
   return {
-    type    : 'accountProof',
-    block   : serialize.blockToHex(block),
+    type: 'accountProof',
+    block: serialize.blockToHex(block),
     accounts: { [address]: account }
   } as Proof
 }
@@ -153,18 +154,18 @@ export async function updateNodeList(handler: RPCHandler, list: ServerList, last
   }
 
   // number of registered servers
-  const [serverCount]  = await tx.callContract(handler.config.rpcUrl, list.contract, 'totalServers():(uint)', [])
+  const [serverCount] = await tx.callContract(handler.config.rpcUrl, list.contract, 'totalServers():(uint)', [])
   list.lastBlockNumber = lastBlockNumber || parseInt(await handler.getFromServer({ method: 'eth_blockNumber', params: [] }).then(_ => _.result as string))
-  list.totalServers    = serverCount.toNumber()
+  list.totalServers = serverCount.toNumber()
 
   // build the requests per server-entry
   const nodeRequests: RPCRequest[] = []
   for (let i = 0; i < serverCount.toNumber(); i++)
     nodeRequests.push({
       jsonrpc: '2.0',
-      id     : i + 1,
-      method : 'eth_call', params: [{
-        to  : list.contract,
+      id: i + 1,
+      method: 'eth_call', params: [{
+        to: list.contract,
         data: '0x' + abi.simpleEncode('servers(uint)', toHex(i, 32)).toString('hex')
       },
         'latest']
@@ -173,15 +174,16 @@ export async function updateNodeList(handler: RPCHandler, list: ServerList, last
   list.nodes = await handler.getAllFromServer(nodeRequests).then(all => all.map((n, i) => {
     // invalid requests must be filtered out
     if (n.error) return null
-    const [url, owner, deposit, props, unregisterTime] = abi.simpleDecode('servers(uint):(string,address,uint,uint,uint,address)', toBuffer(n.result))
+    const [url, owner, timeout, deposit, props, unregisterTime] = abi.simpleDecode('servers(uint):(string,address,uint64,uint,uint,uint,address)', toBuffer(n.result))
 
     return {
       url,
-      address              : toChecksumAddress(owner),
-      index                : i,
-      deposit              : parseInt(deposit.toString()),
-      props                : props.toNumber(),
-      chainIds             : [handler.chainId],
+      address: toChecksumAddress(owner),
+      timeout: parseInt(timeout.toString()),
+      index: i,
+      deposit: parseInt(deposit.toString()),
+      props: props.toNumber(),
+      chainIds: [handler.chainId],
       unregisterRequestTime: unregisterTime.toNumber()
     } as IN3NodeConfig
 
