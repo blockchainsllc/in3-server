3f5289161713426713fea3b353ee3c01023218f2
Martin Küchler <mkuechle@hs-mittweida.de>
2019-03-25 17:35:28 +0100

    comments to blockHash-Registry

--- contracts/BlockhashRegistry.sol
+++ contracts/BlockhashRegistry.sol
@@ -1,24 +1,29 @@
 pragma solidity ^0.5.4;
 pragma experimental ABIEncoderV2;
 
+/// @title Registry for blockhashes
 contract BlockhashRegistry {
 
+    /// a new blockhash and its number has been added to the contract
     event LogBlockhashAdded(uint indexed blockNr, bytes32 indexed bhash);
 
+    /// maps the blocknumber to its blockhash
     mapping(uint => bytes32) public blockhashMapping;
 
+    /// constructor, calls snapshot-function when contract get deployed as entry point
     constructor() public {
         snapshot();
     }
 
-    // stores a blockhash to the state
+    /// stores the currentBlock-1 in the smart contract
     function snapshot() public {
 
-        // blockhash cannot return the current block, so we use the block before the current one
+        /// blockhash cannot return the current block, so we use the block before the current one
         saveBlockNumber(block.number-1);
     }
 
-    // stores a certain blockhash to the state
+    /// stores a certain blockhash to the state
+    /// will fail if the block can't be found inside the evm
     function saveBlockNumber(uint _blockNumber) public {
         bytes32 bHash = blockhash(_blockNumber);
 
@@ -26,13 +31,17 @@ contract BlockhashRegistry {
 
         blockhashMapping[_blockNumber] = bHash;
         emit LogBlockhashAdded(_blockNumber, bHash);
-
     }
 
+    /// returns the blockhash and the parent blockhash from the provided blockheader
     function getParentAndBlockhash(bytes memory _blockheader) public pure returns (bytes32 parentHash, bytes32 bhash) {
         
+        /// we need the 1st byte of the blockheader to calculate the position of the parentHash
         uint8 first = uint8(_blockheader[0]);
 
+        /// calculates the offset
+        /// by using the 1st byte (usually f9) and substracting f7 to get the start point of the parentHash information
+        /// er also have to add "2" = 1 byte to it to skip the length-information 
         uint8 offset = first-0xf7+2;
         
         assembly {
@@ -54,14 +63,18 @@ contract BlockhashRegistry {
         bhash = keccak256(_blockheader);
     }
 
-    // starts with a given blockhash and adds earlier blocks
+    /// starts with a given blockhash and its header and tries to recreate a (reverse) chain of blocks
+    /// the array of the blockheaders have to be in reverse order (e.g. [100,99,98,97])
+    /// if the provided chain is not correct (parentHash != calculated hash)
+    /// if the hash is correct it will return the blockhash of the last header
     function calculateBlockheaders(bytes[] memory _blockheaders, bytes32 _bHash) public pure returns (bytes32 bhash) {
 
         bytes32 currentBlockhash = _bHash;
         bytes32 calcParent = 0x0;
         bytes32 calcBlockhash = 0x0;
-        for(uint i = 0; i < _blockheaders.length; i++) {
 
+        /// save to use for up to 200 blocks, exponential increase of gas-usage afterwards
+        for(uint i = 0; i < _blockheaders.length; i++) {
             (calcParent, calcBlockhash) = getParentAndBlockhash(_blockheaders[i]);
             if(calcBlockhash != currentBlockhash) return 0x0;
             currentBlockhash = calcParent;
@@ -70,6 +83,10 @@ contract BlockhashRegistry {
         return currentBlockhash;
     }
 
+    /// starts with a given blocknumber and its header and tries to recreate a (reverse) chain of blocks 
+    /// only usable when the given blocknumber is already in the smart contract
+    /// it will be checked whether the provided chain is correct by using the calculateBlockheaders function
+    /// if successfull the last blockhash of the header will be added to the smart contract
     function recreateBlockheaders(uint _blockNumber, bytes[] memory _blockheaders) public {
         bytes32 currentBlockhash = blockhashMapping[_blockNumber];
         require(currentBlockhash != 0x0, "parentBlock is not available");
