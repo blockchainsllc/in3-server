a0bd906d661fd95107a081705f48670bc5f6db2f
pari <parithosh@slock.it>
2019-06-11 15:52:05 +0200

    Updating node version

--- src/modules/eth/threadPool.ts
+++ src/modules/eth/threadPool.ts
@@ -1,4 +1,5 @@
 import { Worker } from 'worker_threads'
+const fs = require('fs').promises;
 
 let workers = []
 let firstTime = true
@@ -42,7 +43,7 @@ class ThreadPool {
         if (this.hasWorkers()) {
             return await workers.shift()
         } else {
-            workers.push({ "worker": new Worker('./merkle.js'), "lastInteraction": Date.now() })
+            workers.push({ "worker": new Worker(process.cwd() + '/js/src/modules/eth/merkle.js'), "lastInteraction": Date.now() })
             return await workers.shift()
         }
     }
