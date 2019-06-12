a7055ca04bb328032630194e0e8862700ce5bb2a
Martin Küchler <mkuechle@hs-mittweida.de>
2019-06-12 14:31:31 +0200

    function ordering according to style guide

--- contracts/BlockhashRegistry.sol
+++ contracts/BlockhashRegistry.sol
@@ -1,4 +1,23 @@
-pragma solidity ^0.5.7;
+/***********************************************************
+* This file is part of the Slock.it IoT Layer.             *
+* The Slock.it IoT Layer contains:                         *
+*   - USN (Universal Sharing Network)                      *
+*   - INCUBED (Trustless INcentivized remote Node Network) *
+************************************************************
+* Copyright (C) 2016 - 2018 Slock.it GmbH                  *
+* All Rights Reserved.                                     *
+************************************************************
+* You may use, distribute and modify this code under the   *
+* terms of the license contract you have concluded with    *
+* Slock.it GmbH.                                           *
+* For information about liability, maintenance etc. also   *
+* refer to the contract concluded with Slock.it GmbH.      *
+************************************************************
+* For more information, please refer to https://slock.it   *
+* For questions, please contact info@slock.it              *
+***********************************************************/
+
+pragma solidity ^0.5.9;
 pragma experimental ABIEncoderV2;
 
 /// @title Registry for blockhashes
@@ -15,15 +34,38 @@ contract BlockhashRegistry {
         snapshot();
     }
 
-    /// stores the currentBlock-1 in the smart contract
-    function snapshot() public {
+    /// searches for an already existing snapshot 
+    /// @param _startNumber the blocknumber to start searching
+    /// @param _numBlocks the number of blocks to search for
+    /// @return the closes snapshot of found within the given range, 0 else
+    function searchForAvailableBlock(uint _startNumber, uint _numBlocks) external view returns (uint) {
 
-        /// blockhash cannot return the current block, so we use the block before the current one
-        saveBlockNumber(block.number-1);
+        for(uint i = _startNumber; i <= (_numBlocks + _startNumber); i++){
+           if(blockhashMapping[i] != 0x0) return i; 
+        }
+    }
+
+    /// starts with a given blocknumber and its header and tries to recreate a (reverse) chain of blocks 
+    /// only usable when the given blocknumber is already in the smart contract
+    /// it will be checked whether the provided chain is correct by using the calculateBlockheaders function
+    /// if successfull the last blockhash of the header will be added to the smart contract
+    /// @param _blockNumber the block number to start recreation from
+    /// @param _blockheaders array with serialized blockheaders in reverse order (youngest -> oldest) => (e.g. 100, 99, 98)
+    function recreateBlockheaders(uint _blockNumber, bytes[] memory _blockheaders) public {
+        bytes32 currentBlockhash = blockhashMapping[_blockNumber];
+        require(currentBlockhash != 0x0, "parentBlock is not available");
+
+        bytes32 calculatedHash = calculateBlockheaders(_blockheaders, currentBlockhash);
+        require(calculatedHash != 0x0, "invalid headers");
+
+        uint bnr = _blockNumber - _blockheaders.length;
+        blockhashMapping[bnr] = calculatedHash;
+        emit LogBlockhashAdded(bnr, calculatedHash);
     }
 
     /// stores a certain blockhash to the state
     /// will fail if the block can't be found inside the evm
+    /// @param _blockNumber the blocknumber to be stored
     function saveBlockNumber(uint _blockNumber) public {
         bytes32 bHash = blockhash(_blockNumber);
 
@@ -33,7 +75,39 @@ contract BlockhashRegistry {
         emit LogBlockhashAdded(_blockNumber, bHash);
     }
 
+    /// stores the currentBlock-1 in the smart contract
+    function snapshot() public {
+
+        /// blockhash cannot return the current block, so we use the previous block
+        saveBlockNumber(block.number-1);
+    }
+
+    /// starts with a given blockhash and its header and tries to recreate a (reverse) chain of blocks
+    /// the array of the blockheaders have to be in reverse order (e.g. [100,99,98,97])
+    /// if the provided chain is not correct (parentHash != calculated hash)
+    /// if the hash is correct it will return the blockhash of the last header
+    /// @param _blockheaders array with serialized blockheaders in reverse order, i.e. from youngest to oldest
+    /// @param _bHash blockhash of the 1st element of the _blockheaders-array
+    /// @return 0x0 if the functions detects a wrong chaining of blocks, blockhash of the last element of the array otherwhise
+    function calculateBlockheaders(bytes[] memory _blockheaders, bytes32 _bHash) public pure returns (bytes32 bhash) {
+
+        bytes32 currentBlockhash = _bHash;
+        bytes32 calcParent = 0x0;
+        bytes32 calcBlockhash = 0x0;
+
+        /// save to use for up to 200 blocks, exponential increase of gas-usage afterwards
+        for(uint i = 0; i < _blockheaders.length; i++) {
+            (calcParent, calcBlockhash) = getParentAndBlockhash(_blockheaders[i]);
+            if(calcBlockhash != currentBlockhash) return 0x0;
+            currentBlockhash = calcParent;
+        }
+
+        return currentBlockhash;
+    }
+
     /// returns the blockhash and the parent blockhash from the provided blockheader
+    /// @param _blockheader a serialized (rlp-encoded) blockheader
+    /// @return the parent blockhash and the keccak256 of the provided blockheader (= the corresponding blockhash)
     function getParentAndBlockhash(bytes memory _blockheader) public pure returns (bytes32 parentHash, bytes32 bhash) {
         
         /// we need the 1st byte of the blockheader to calculate the position of the parentHash
@@ -63,48 +137,4 @@ contract BlockhashRegistry {
         bhash = keccak256(_blockheader);
     }
 
-    /// starts with a given blockhash and its header and tries to recreate a (reverse) chain of blocks
-    /// the array of the blockheaders have to be in reverse order (e.g. [100,99,98,97])
-    /// if the provided chain is not correct (parentHash != calculated hash)
-    /// if the hash is correct it will return the blockhash of the last header
-    function calculateBlockheaders(bytes[] memory _blockheaders, bytes32 _bHash) public pure returns (bytes32 bhash) {
-
-        bytes32 currentBlockhash = _bHash;
-        bytes32 calcParent = 0x0;
-        bytes32 calcBlockhash = 0x0;
-
-        /// save to use for up to 200 blocks, exponential increase of gas-usage afterwards
-        for(uint i = 0; i < _blockheaders.length; i++) {
-            (calcParent, calcBlockhash) = getParentAndBlockhash(_blockheaders[i]);
-            if(calcBlockhash != currentBlockhash) return 0x0;
-            currentBlockhash = calcParent;
-        }
-
-        return currentBlockhash;
-    }
-
-    /// starts with a given blocknumber and its header and tries to recreate a (reverse) chain of blocks 
-    /// only usable when the given blocknumber is already in the smart contract
-    /// it will be checked whether the provided chain is correct by using the calculateBlockheaders function
-    /// if successfull the last blockhash of the header will be added to the smart contract
-    function recreateBlockheaders(uint _blockNumber, bytes[] memory _blockheaders) public {
-        bytes32 currentBlockhash = blockhashMapping[_blockNumber];
-        require(currentBlockhash != 0x0, "parentBlock is not available");
-
-        bytes32 calculatedHash = calculateBlockheaders(_blockheaders, currentBlockhash);
-        require(calculatedHash != 0x0, "invalid headers");
-
-        uint bnr = _blockNumber - _blockheaders.length;
-        blockhashMapping[bnr] = calculatedHash;
-        emit LogBlockhashAdded(bnr, calculatedHash);
-    }
-
-    /// returns the closest snapshot if found within the given range
-    /// returns 0 if no snapshot has been found in that rnage
-    function searchForAvailableBlock(uint _startNumber, uint _numBlocks) external view returns (uint) {
-
-        for(uint i = _startNumber; i <= (_numBlocks + _startNumber); i++){
-           if(blockhashMapping[i] != 0x0) return i; 
-        }
-    }
 }
