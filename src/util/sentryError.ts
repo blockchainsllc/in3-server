1fef3865c6cc93fc8daf4a938e3453bbc3a437b3
pari <parithosh@slock.it>
2019-07-04 12:13:35 +0200

    testing stack trace as variable

--- src/util/sentryError.ts
+++ src/util/sentryError.ts
@@ -2,10 +2,11 @@ const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://1aca629ca89c42a6b5601fcce6499103@sentry.slock.it/5' });
 
 export class SentryError extends Error {
-    constructor(message? :string) {
+    constructor(message? :string, stack? :string) {
         super(message);
+        super(stack);
 
-        Error.captureStackTrace(this,SentryError)
-        Sentry.captureException(this)
+        // Error.captureStackTrace(this,SentryError)
+        Sentry.captureException(stack)
     }
 }
