b25d913159df219f594dd71c70edb59b8959974a
pari <parithosh@slock.it>
2019-07-04 11:52:55 +0200

    attempting stack trace

--- src/util/sentryError.ts
+++ src/util/sentryError.ts
@@ -2,8 +2,11 @@ const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://1aca629ca89c42a6b5601fcce6499103@sentry.slock.it/5' });
 
 export class SentryError extends Error {
-    constructor(message?: string) {
-        super(message)
-        Sentry.captureException(message)
+    constructor(message, meta = {}) {
+        super(message);
+
+        Error.captureStackTrace(this,SentryError)
+        this.meta = meta
+        Sentry.captureException(this)
     }
 }
