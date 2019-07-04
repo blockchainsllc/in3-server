ded744a199cf7f1faee100045887b641ae7df12a
pari <parithosh@slock.it>
2019-07-04 13:06:41 +0200

    removed all changes using stack, didnt seem to work

--- src/util/sentryError.ts
+++ src/util/sentryError.ts
@@ -2,10 +2,8 @@ const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://1aca629ca89c42a6b5601fcce6499103@sentry.slock.it/5' });
 
 export class SentryError extends Error {
-    constructor(message? :string, stack? :string) {
+    constructor(message? :string) {
         super(message);
-        console.log(stack)
-        console.log(Error.captureStackTrace(this,SentryError))
         Sentry.captureException(message)
     }
 }
