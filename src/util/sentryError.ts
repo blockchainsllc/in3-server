c19da3f0aea9acf8c0c36ba05372b7bf799d102c
pari <parithosh@slock.it>
2019-07-09 10:52:16 +0200

    modified var names for readability

--- src/util/sentryError.ts
+++ src/util/sentryError.ts
@@ -2,13 +2,12 @@ const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://1aca629ca89c42a6b5601fcce6499103@sentry.slock.it/5' });
 
 export class SentryError extends Error {
-    constructor(message? :string, tmp? :string,tmp_data? :string) {
+    constructor(message? :string, category_info? :string, breadcrumb_message? :string) {
         super(message);
-        console.log(message)
 
         Sentry.addBreadcrumb({
-            category: tmp,
-            message: tmp_data,
+            category: category_info,
+            message: breadcrumb_message,
         });
 
         Sentry.captureException(message)
