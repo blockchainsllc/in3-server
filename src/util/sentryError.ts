b72ea42572f82870b9e9a277d04b9ea890c707c7
Simon Jentzsch <simon@slock.it>
2019-07-17 09:15:09 +0000

    Update sentryError.ts

--- src/util/sentryError.ts
+++ src/util/sentryError.ts
@@ -1,5 +1,11 @@
 const Sentry = require('@sentry/node');
-
+/**
+ * creates a Error with the capability to report it to Sentry.
+ * Whether the error is reported depends on the enviroment variable `SENTRY_ENABLE`.
+ * 
+ * For more details, see 
+ * https://git.slock.it/documentation/developer-handbook/blob/master/docs/Error-handling-and-reporting-Sentry.md
+ */
 export class SentryError extends Error {
     constructor(message?: string, category_info?: string, breadcrumb_message?: string) {
         super(message);
