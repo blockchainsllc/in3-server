5d917926c93a584bc3ba2447505590aa3e3cc0bc
Markus Keil <markus.keil@slock.it>
2019-07-30 09:28:37 +0200

    improve sentry integration and enable on deploy

--- src/util/sentryError.ts
+++ src/util/sentryError.ts
@@ -31,9 +31,6 @@ export class SentryError extends Error {
     constructor(message?: string, category_info?: string, breadcrumb_message?: string) {
         super(message);
         if (process.env.SENTRY_ENABLE === 'true') {
-
-            Sentry.init({dsn: process.env.SENTRY_DSN})
-
             Sentry.addBreadcrumb({
                 category: category_info,
                 message: breadcrumb_message,
