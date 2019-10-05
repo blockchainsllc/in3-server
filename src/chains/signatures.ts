e4422ab8523d2d10c6c0f03f21026c1271df3b7d
Simon Jentzsch <simon@slock.it>
2019-10-05 14:19:27 +0200

    add double check before convict

--- src/chains/signatures.ts
+++ src/chains/signatures.ts
@@ -53,6 +53,17 @@ const bytes = serialize.bytes
 
 export const signatureCaches: LRUCache = new LRUCache();
 
+function checkBlockHash(hash: any, expected: any, s: any) {
+  // is the blockhash correct all is fine
+  if (bytes32(hash).equals(bytes32(expected))) {
+    // add signature entry in cache
+    if (!signatureCaches.has(expected))
+      signatureCaches.set(expected, { ...s })
+    return s
+  }
+  return null
+}
+
 export async function collectSignatures(handler: BaseHandler, addresses: string[], requestedBlocks: { blockNumber: number, hash?: string }[], verifiedHashes: string[]): Promise<Signature[]> {
   // DOS-Protection
   if (addresses && addresses.length > config.maxSignatures) throw new Error('Too many signatures requested!')
@@ -130,22 +141,21 @@ export async function collectSignatures(handler: BaseHandler, addresses: string[
           return null
         }
 
+        // did we expect this?
+        if (checkBlockHash(s.blockHash, expectedBlock.hash, s)) return s
 
-        // is the blockhash correct all is fine
-        if (bytes32(s.blockHash).equals(bytes32(expectedBlock.hash))) {
-          // add signature entry in cache
-          if (!signatureCaches.has(expectedBlock.hash))
-            signatureCaches.set(expectedBlock.hash, { ...s })
+        // so we have a different hash, let's double check if got the wrong hash
+        s.blockHash = toHex(await handler.getFromServer({ method: 'eth_getBlockByNumber', params: [toMinHex(s.block), false] })
+          .then(_ => _.result && _.result.hash), 32)
 
-          return s
-        }
+        // recheck again, if this is still wrong
+        if (checkBlockHash(s.blockHash, expectedBlock.hash, s)) return s
 
+        // ok still wrong, so we start convicting the node...
         logger.info("Trying to convict node(" + singingNode.address + ") " + singingNode.url + ' because it signed wrong blockhash  with ' + JSON.stringify(s) + ' but the correct hash should be ' + expectedBlock.hash)
 
         const latestBlockNumber = handler.watcher.block.number
-
         const diffBlocks = toNumber(latestBlockNumber) - s.block
-
         const convictSignature: Buffer = keccak(Buffer.concat([bytes32(s.blockHash), address(singingNode.address), toBuffer(s.v, 1), bytes32(s.r), bytes32(s.s)]))
 
         if (diffBlocks < 255) {
