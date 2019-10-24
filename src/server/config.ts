230e04ad625fa945e1e24393a7f28d5177dee0cb
Simon Jentzsch <simon@slock.it>
2019-10-24 10:51:22 +0200

    optimized argument handling

--- src/server/config.ts
+++ src/server/config.ts
@@ -126,23 +126,13 @@ export function readCargs(): IN3RPCConfig {
   const processedArgs = vals.parse(process.argv, { mri: { string: options.map(_ => _.name) } })
 
   // fix chainIds to minHex and enable or disable cache
-  for (let c of Object.keys(config.chains)) {
-    const min = util.toMinHex(c)
-    if (min != c) {
-      config.chains[min] = config.chains[c]
-      delete config.chains[c]
-      c = min
-    }
+  Object.keys(config.chains).filter(_ => util.toMinHex(_) != _).forEach(c => {
+    config.chains[util.toMinHex(c)] = config.chains[c]
+    delete config.chains[c]
+  })
 
-    //explicit command must be specified to disable cache else it is enabled
-    if (processedArgs.cache === 'false') {
-      (config.chains[c] as any).useCache = false
-    }
-    else {
-      (config.chains[c] as any).useCache = true
-    }
-
-  }
+  // set the cache
+  Object.keys(config.chains).forEach(c => (config.chains[c] as any).useCache = processedArgs.cache !== 'false')
 
   return config
 }
