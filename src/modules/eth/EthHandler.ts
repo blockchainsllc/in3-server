60481eb898180318fbe852549249cc03e4f2a910
junaidtaj <junaid.taj@slock.it>
2019-10-28 21:17:35 +0100

    adding new incoming whitelist addr in watch

--- src/modules/eth/EthHandler.ts
+++ src/modules/eth/EthHandler.ts
@@ -122,6 +122,10 @@ export default class EthHandler extends BaseHandler {
     // check performancelimits
     this.checkPerformanceLimits(request)
 
+    if(request.in3 && request.in3.whiteList){
+      //TODO basic validation here
+      this.watcher.addWhiteListWatch(request.in3.whiteList)
+    }
 
     // handle special jspn-rpc
     if (request.in3.verification == 'proof' || request.in3.verification == 'proofWithSignature') // proofWithSignature is only supported for legacy, since only the request for signers is relveant
