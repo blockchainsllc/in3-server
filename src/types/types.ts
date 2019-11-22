507bf5abcb08f7544ba6af19799a4e76dbf41bde
junaidtaj <junaid.taj@slock.it>
2019-11-22 21:36:20 +0100

    added comments to type recently added and added type in yaml file

--- src/types/types.ts
+++ src/types/types.ts
@@ -127,7 +127,10 @@ export interface IN3RPCRequestConfig {
      * example: 1.0.0
      */
     version?: string
-
+    /**
+     * address of whitelist contract if added in3 server will register it in watch 
+     * and will notify client the whitelist event block number in reponses it depends on cahce settings
+     */
     whiteList?: string
 }
 /**
