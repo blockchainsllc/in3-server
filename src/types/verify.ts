0c7339527610941e2cc1dc0798baf9c93a027588
Simon Jentzsch <simon@slock.it>
2019-11-29 12:27:41 +0100

    added Sentry

--- src/types/verify.ts
+++ src/types/verify.ts
@@ -1,3 +1,39 @@
+/*******************************************************************************
+ * This file is part of the Incubed project.
+ * Sources: https://github.com/slockit/in3-server
+ * 
+ * Copyright (C) 2018-2019 slock.it GmbH, Blockchains LLC
+ * 
+ * 
+ * COMMERCIAL LICENSE USAGE
+ * 
+ * Licensees holding a valid commercial license may use this file in accordance 
+ * with the commercial license agreement provided with the Software or, alternatively, 
+ * in accordance with the terms contained in a written agreement between you and 
+ * slock.it GmbH/Blockchains LLC. For licensing terms and conditions or further 
+ * information please contact slock.it at in3@slock.it.
+ * 	
+ * Alternatively, this file may be used under the AGPL license as follows:
+ *    
+ * AGPL LICENSE USAGE
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * [Permissions of this strong copyleft license are conditioned on making available 
+ * complete source code of licensed works and modifications, which include larger 
+ * works using a licensed work, under the same license. Copyright and license notices 
+ * must be preserved. Contributors provide an express grant of patent rights.]
+ * You should have received a copy of the GNU Affero General Public License along 
+ * with this program. If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+const Sentry = require('@sentry/node');
+
 import * as Ajv from 'ajv'
 
 // the schema
@@ -16,10 +52,8 @@ export function verifyRequest(req: any) {
         throw new Error(req.method + ' : ' + getErrorMessage(ajv.errors, schema.definitions[req.method], null, req))
 }
 function getErrorMessage(errs: Ajv.ErrorObject[], s?: any, opt?: any, data?: any) {
-    //    console.log("request:", JSON.stringify(data, null, 2))
-    //    console.log('errors:' + JSON.stringify(errs, null, 2))
     const all = [...errs]
-    const ones = errs.filter(_ => _.keyword == 'oneOf').map(one => {
+    errs.filter(_ => _.keyword == 'oneOf').map(one => {
         one.message = 'must be ' + all.filter(_ => _ !== one && _.schemaPath.startsWith(one.schemaPath)).map(sub => {
             const i = all.indexOf(sub)
             all.splice(i, 1)
@@ -52,7 +86,17 @@ function getErrorMessage(errs: Ajv.ErrorObject[], s?: any, opt?: any, data?: any
     }
 
     const msg = ajv.errorsText(all, opt || { dataVar: 'params' })
-    //    console.log("Message=", msg)
+
+    // register with sentry
+    if (process.env.SENTRY_ENABLE === 'true') {
+        Sentry.addBreadcrumb({
+            request: data,
+            message: msg,
+            errs
+        })
+        Sentry.captureException('Invalid Userdata')
+    }
+
     return msg
 }
 
