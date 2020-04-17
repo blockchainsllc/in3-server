0f99db682874d162d8049f0e406670454a3142e3
junaidtaj <junaid.taj@slock.it>
2020-04-17 15:22:38 +0200

    found a bug, incase user is specifying only one rpcRUrl then arg was treated as string instead of array, fixed that

--- src/server/config.ts
+++ src/server/config.ts
@@ -86,6 +86,9 @@ function parseDef(def: { properties: any, type: string }, targetPath = [], targe
         init: v => {
           const t = targetPath.reduce((t, pp) => t[pp] || (t[pp] = {}), targetOb)
 
+          if(p=='rpcUrl' && val.type=='array' && !Array.isArray(v))
+            return t[p] = new Array(v)
+
           switch (val.type) {
             case 'number':
             case 'integer':
@@ -102,7 +105,12 @@ function parseDef(def: { properties: any, type: string }, targetPath = [], targe
 }
 
 export function readCargs(): IN3RPCConfig {
-
+  console.log("sleeping...")
+  var now = new Date().getTime();
+  var millisecondsToWait = 8000; /* i.e. 1 second */
+  //Using while loop:
+  while ( new Date().getTime() < now + millisecondsToWait ){}
+  //////////
   // take the config from config.json and overwrite it
   try {
     Object.assign(config, JSON.parse(fs.readFileSync('config.json', 'utf-8')))
